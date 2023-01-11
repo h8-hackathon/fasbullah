bc22ddf20ad8d49807567329667c005ba219b1ab
ardianrivaulhak <ardianrivaulhak@gmail.com>
2023-01-12 01:17:33 +0700

    test ejs

--- app.js
+++ app.js
@@ -3,7 +3,25 @@ const express = require('express')
 const app = express()
 const PORT = 3000
 
-app.get('/', (req, res) => res.send('home'))
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+
+
+app.get('/', (req, res) => {
+    res.render('user/index')
+})
+
+app.get('/login', (req, res) => {
+    res.render('user/login')
+})
+
+app.get('/register', (req, res) => {
+    res.render('user/register')
+})
+
+app.get('/post', (req, res) => {
+    res.render('post/post')
+})
 
 app.listen(PORT)
 
